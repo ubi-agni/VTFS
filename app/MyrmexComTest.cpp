422d8aad9455191e1000c6080802d63fa5e67da2
Qiang Li <qli@TechFak.Uni-Bielefeld.DE>
2016-10-26 11:42:02 +0200

    unified mymrex communication test

--- app/MyrmexComTest.cpp
+++ app/MyrmexComTest.cpp
@@ -13,6 +13,7 @@
 
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 #include "Timer.h"
 #include "comrsb.h"
@@ -23,40 +24,66 @@
 
 
 ComRSB *com_rsb;
+RsbDataType rdtrightkuka;
+RsbDataType rdtrighttac;
 RsbDataType rdtleftkuka;
 RsbDataType rdtlefttac;
-myrmex_msg left_myrmex_msg;
-kuka_msg left_kuka_msg;
+myrmex_msg m_myrmex_msg;
+kuka_msg kuka_msg;
+std::string myrmex_name;
 
 void tactileviarsb(){
     //via network--RSB, the contact information are obtained.
-    com_rsb->tactile_receive(left_myrmex_msg,"leftmyrmex");
+    if(myrmex_name == "rmyrmex"){
+    com_rsb->tactile_receive(m_myrmex_msg,"rightmyrmex");
     std::cout<<"tactile output"<<std::endl;
-    std::cout<<"myrmex readout "<<left_myrmex_msg.cogx<<","<<left_myrmex_msg.cogy<<std::endl;
+    std::cout<<"right myrmex readout "<<m_myrmex_msg.cogx<<","<<m_myrmex_msg.cogy<<std::endl;
+    }
+    else{
+        com_rsb->tactile_receive(m_myrmex_msg,"leftmyrmex");
+        std::cout<<"tactile output"<<std::endl;
+        std::cout<<"left myrmex readout "<<m_myrmex_msg.cogx<<","<<m_myrmex_msg.cogy<<std::endl;
+    }
 }
 
 void init(){
     com_rsb = new ComRSB();
-    rdtleftkuka = LeftKukaEff;
-    rdtlefttac = LeftMyrmex;
-    com_rsb->add_msg(rdtleftkuka);
-    com_rsb->add_msg(rdtlefttac);
+    if(myrmex_name == "rmyrmex"){
+        rdtrightkuka = RightKukaEff;
+        rdtrighttac = RightMyrmex;
+        com_rsb->add_msg(rdtrightkuka);
+        com_rsb->add_msg(rdtrighttac);
+    }
+    else{
+        rdtleftkuka = LeftKukaEff;
+        rdtlefttac = LeftMyrmex;
+        com_rsb->add_msg(rdtleftkuka);
+        com_rsb->add_msg(rdtlefttac);
+    }
 }
 
 
 int main(int argc, char* argv[])
 {
-    init();
-    //start myrmex read thread
-    Timer thrd_myrmex_read(tactileviarsb);
-    thrd_myrmex_read.setSingleShot(false);
-    thrd_myrmex_read.setInterval(Timer::Interval(5));
-    thrd_myrmex_read.start(true);
-    //main thread is hanging
-    while(1){
+    if (argc > 1) {
+        myrmex_name = argv[1];
+        init();
+        std::cout<<"start"<<std::endl;
+        //start myrmex read thread
+        Timer thrd_myrmex_read(tactileviarsb);
+        thrd_myrmex_read.setSingleShot(false);
+        thrd_myrmex_read.setInterval(Timer::Interval(5));
+        thrd_myrmex_read.start(true);
+        //main thread is hanging
+        while(1){
+        }
+        thrd_myrmex_read.stop();
+        return 0;
     }
-    thrd_myrmex_read.stop();
-    return 0;
+    else{
+        std::cout<<"please specify which myrmex is used"<<std::endl;
+        return 1;
+    }
 }
 
 
